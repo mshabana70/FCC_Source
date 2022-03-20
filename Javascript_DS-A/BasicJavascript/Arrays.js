bd75fc1b0ddfc30e656df397d5c17866b7984351
Mahmoud Shabana <mshabana5656@gmail.com>
2022-03-20 19:28:58 -0400

    learning about arrays

---
+++ Javascript_DS-A/BasicJavascript/Arrays.js
@@ -0,0 +1,11 @@
+// JAVASCRIPT ARRAYS
+/**
+ * With Array variables, you can store multiple
+ * pieces of data in one place.
+ */
+
+// All arrays are declared with an open and closing square bracket
+const sandwich = ["Peanut Butter", "Jelly", "Bread"];
+
+// Lets see whats inside this array using indexes
+console.log("To make a sandwich you need " + sandwich[0] + ", " + sandwich[1] + ", and " + sandwich[2]);
