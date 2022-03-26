405256255e803fb8724dbdcd7da784e9cc37eaec
Mahmoud Shabana <mshabana5656@gmail.com>
2022-03-26 16:57:05 -0400

    conditional example

--- Javascript_DS-A/BasicJavascript/conditional.js
+++ Javascript_DS-A/BasicJavascript/conditional.js
@@ -3,4 +3,23 @@
 // If statements are used to make decisions in code.
 // They usually help direct the flow of your code as
 // your script runs.
- 
+
+/* Syntax of a Conditional If statement
+if (condition is true) {
+    statement is executed
+}
+*/
+
+// Example
+
+// Create function with conditional
+function test(myCondition) {
+    if (myCondition) {
+        return "It was true";
+    }
+    return "It was false";
+}
+
+// Call function to test conditional
+test(true);
+test(false);
