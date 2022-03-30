30629d1590571c56ac01fdcbd52c3e7ffe53af28
Mahmoud Shabana <mshabana5656@gmail.com>
2022-03-30 18:03:04 -0400

    while loop example and notes

--- Javascript_DS-A/BasicJavascript/loop.js
+++ Javascript_DS-A/BasicJavascript/loop.js
@@ -1,5 +1,5 @@
 // JAVASCRIPT LOOPS
-
+console.log("\n/*************** JS LOOPS ***************/\n")
 /*
     In programming, when it comes to tasks that 
     require certain commands or blocks of code to be
@@ -8,3 +8,29 @@
     a set amount of iterations, JavaScript has you covered with
     different loops to depending on what you need.
 */
+
+// WHILE LOOPS
+console.log("\n/*************** WHILE LOOPS ***************/\n")
+/*
+    While loops run blocks of code while a specified condition
+    is true. As long as that condition is true, you code block will
+    rerun until it is not.
+
+    NOTE: These forms of loops are prone to issues as it is easy to
+    mistakely create what is known as an "Infinite Loop." Infinite loops
+    will run infinitely, because there is nothing stoping the condition
+    that was specified from being true.
+*/
+
+// Example of while loops
+const myArray = [];
+
+// Fill myArray from 5 to 0 (inclusive)
+var i = 5;
+while (i >= 0) {
+    myArray.push(i);
+    i--;
+}
+
+// Return myArray
+console.log(myArray); // [5, 4, 3, 2, 1, 0]
