ac0bd00604aae312502429c5b285d812aed2a3b9
Mahmoud Shabana <mshabana5656@gmail.com>
2022-03-30 17:45:03 -0400

    Loops notes summary

--- Javascript_DS-A/BasicJavascript/loop.js
+++ Javascript_DS-A/BasicJavascript/loop.js
@@ -1,1 +1,10 @@
 // JAVASCRIPT LOOPS
+
+/*
+    In programming, when it comes to tasks that 
+    require certain commands or blocks of code to be
+    run continously, loops are the best for that. Depending
+    on if you need to run while a condition is met, or if you need
+    a set amount of iterations, JavaScript has you covered with
+    different loops to depending on what you need.
+*/
